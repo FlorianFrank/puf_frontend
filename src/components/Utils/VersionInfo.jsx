073abbca1213f3d7e6f978330ca0c2cfe8751cf0
Florian Frank <Florian.Frank@uni-passau.de>
2024-01-16 20:08:41 +0100

    Collapse unnecessary td tags in dvelopers field

--- src/components/Utils/VersionInfo.jsx
+++ src/components/Utils/VersionInfo.jsx
@@ -4,10 +4,9 @@ import Typography from "@mui/material/Typography";
 
 const VersionInfo = () => {
 
-// className={classes.paper}
     return (<header className="App-header">
-            <div  style={{  margin: 'auto', width: '50%', paddingTop: '5%'}}>
-                <div className="p-5" style={{  margin: 'auto', width: '80%'}}>
+            <div style={{margin: 'auto', width: '50%', paddingTop: '5%'}}>
+                <div className="p-5" style={{margin: 'auto', width: '80%'}}>
                     <img src={logo} width="400px" alt="logo"/>
                     <Typography variant="h5" color="text.secondary">
                         PUF Execution & Evaluation Platform
@@ -33,42 +32,38 @@ const VersionInfo = () => {
                             </td>
                         </tr>
 
-                    <tr>
-                        <td>
+                        <tr>
+                            <td>
                                 Copyright:
-                        </td>
-                        <td>
+                            </td>
+                            <td>
                                 University of Passau - Chair of Computer Engineering
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <div style={{paddingRight: '20px'}}>Developers:</div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div style={{paddingRight: '20px'}}>Developers:</div>
 
-                        </td>
-                        <td>
+                            </td>
+                            <td>
                                 Florian Frank (<a
                                 href="mailto:Florian.Frank@uni-passau.de">Florian.Frank@uni-passau.de</a>)
-                        </td>
-                    </tr>
+                            </td>
+                        </tr>
 
-                    <tr>
-                        <td>
-
-                        </td>
-                        <td>
-                            Abderrazzak El khayari
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-
-                        </td>
-                        <td>
-                            Alexander Braml (<a
-                            href="mailto:braml@fim.uni-passau.de">braml@fim.uni-passau.de</a>)
-                        </td>
-                    </tr>
+                        <tr>
+                            <td/>
+                            <td>
+                                Abderrazzak El Khayari
+                            </td>
+                        </tr>
+                        <tr>
+                            <td/>
+                            <td>
+                                Alexander Braml (<a
+                                href="mailto:braml@fim.uni-passau.de">braml@fim.uni-passau.de</a>)
+                            </td>
+                        </tr>
                     </Typography>
 
                 </table>
