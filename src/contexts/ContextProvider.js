261c039a755ba73d93a1e421cc64163c710de5c5
Florian Frank <Florian.Frank@uni-passau.de>
2024-01-17 12:42:07 +0100

    Add additional elements to ContextProvider

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -1,73 +1,89 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, {createContext, useContext, useState} from 'react';
 
-const StateContext = createContext();
+export const StateContext = createContext();
 
 const initialState = {
-  userProfile: false,
-  notification: false
+    userProfile: false,
+    notification: false
 };
 
-export const ContextProvider = ({ children }) => {
-  const [activeMenu, setActiveMenu] = useState(true);
-  const [isClicked, setIsClicked] = useState(initialState);
+export const ContextProvider = ({children}) => {
+    const [activeMenu, setActiveMenu] = useState(true);
+    const [isClicked, setIsClicked] = useState(initialState);
 
-  const [socket, setSocket] = useState(null);
-  const [isSocketOpen, setIsSocketOpen] = useState(false);
+    const [socket, setSocket] = useState(null);
+    const [isSocketOpen, setIsSocketOpen] = useState(false);
 
-  const [heatmapSocket, setHeatmapSocket] = useState(null);
+    const [heatmapSocket, setHeatmapSocket] = useState(null);
 
-  const [devices, setDevices] = useState([]);
-  const [connectedDevices, setConnectedDevices] = useState([]);
+    const [devices, setDevices] = useState([]);
+    const [connectedDevices, setConnectedDevices] = useState([]);
 
-  const [currentColor, setCurrentColor] = useState('#03C9D7');
-  const [screenSize, setScreenSize] = useState(undefined);
-  const [currentMode, setCurrentMode] = useState('Light');
-  const [themeSettings, setThemeSettings] = useState(false);
-  const [isInsertedHighlighted, setIsInsertedHighlighted] = useState(false);
-  const [insertedCount, setInsertedCount] = useState(0);
+    const [currentColor, setCurrentColor] = useState('#03C9D7');
+    const [screenSize, setScreenSize] = useState(undefined);
+    const [currentMode, setCurrentMode] = useState('Light');
+    const [themeSettings, setThemeSettings] = useState(false);
+    const [isInsertedHighlighted, setIsInsertedHighlighted] = useState(false);
+    const [insertedCount, setInsertedCount] = useState(0);
+    const [userMenuVisible, setUserMenuVisible] = useState(false);
+    const [notificationMenuVisible, setNotificationMenuVisible] = useState(false);
+    const [notifications, setNotifications] = useState([]);
 
-  const setMode = (e) => {
-    setCurrentMode(e.target.value);
-    localStorage.setItem('themeMode', e.target.value);
-  };
+    const [waferConfig, setWaferConfig] = useState([]);
 
-  const setColor = (color) => {
-    setCurrentColor(color);
-    localStorage.setItem('colorMode', color);
-  };
+    const [testTemplates, setTestTemplates] = useState([]);
 
-  const handleClick = (clicked) =>
-    setIsClicked({ ...initialState, [clicked]: true });
-  return (
-    <StateContext.Provider
-      value={{
-        isInsertedHighlighted,
-        setIsInsertedHighlighted,
-        insertedCount,
-        setInsertedCount,
-        activeMenu,
-        setActiveMenu,
-        currentColor,
-        setSocket,
-        socket,
-        isSocketOpen,
-        setIsSocketOpen,
-        devices,
-        setDevices,
-        connectedDevices,
-        setConnectedDevices,
-        isClicked,
-        setIsClicked,
-        handleClick,
-        screenSize,
-        setScreenSize,
-        heatmapSocket,
-        setHeatmapSocket
-      }}
-    >
-      {children}
-    </StateContext.Provider>
-  );
+
+    const setMode = (e) => {
+        setCurrentMode(e.target.value);
+        localStorage.setItem('themeMode', e.target.value);
+    };
+
+    const setColor = (color) => {
+        setCurrentColor(color);
+        localStorage.setItem('colorMode', color);
+    };
+
+    const handleClick = (clicked) =>
+        setIsClicked({...initialState, [clicked]: true});
+    return (
+        <StateContext.Provider
+            value={{
+                isInsertedHighlighted,
+                setIsInsertedHighlighted,
+                insertedCount,
+                setInsertedCount,
+                activeMenu,
+                setActiveMenu,
+                currentColor,
+                setSocket,
+                socket,
+                isSocketOpen,
+                setIsSocketOpen,
+                devices,
+                setDevices,
+                connectedDevices,
+                setConnectedDevices,
+                isClicked,
+                setIsClicked,
+                handleClick,
+                screenSize,
+                setScreenSize,
+                heatmapSocket,
+                setHeatmapSocket,
+                setUserMenuVisible,
+                userMenuVisible,
+                notificationMenuVisible,
+                setNotificationMenuVisible,
+                waferConfig,
+                setWaferConfig,
+                setTestTemplates,
+                testTemplates
+            }}
+        >
+            {children}
+        </StateContext.Provider>
+    );
 };
 
 export const useStateContext = () => useContext(StateContext);
