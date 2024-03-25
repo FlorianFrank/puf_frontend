c399d9bef18aa3bca265bb0050027182b64b3f2c
Florian Frank <Florian.Frank@uni-passau.de>
2024-03-25 21:37:45 +0100

    Remove static test classes

--- src/data/tests_config.js
+++ src/data/tests_config.js
@@ -1,12 +1,4 @@
+// TODO sollte entfernt werden
 export const pufIDs = [
     11, 12, 13, 21, 22, 23, 31, 32, 33
 ]
-
-// TODO sollte entfernt werden
-export const testClasses = {
-    'Memory Tests': '/addMemoryTest',
-    'Script Tests': '/addScriptTest',
-    'Visual Programming Tests': '/addGraphicalTest',
-    'Carbon Nanotube Tests': '/addCNTTest',
-    'Memristor Tests': '/addMemristorTest',
-}
